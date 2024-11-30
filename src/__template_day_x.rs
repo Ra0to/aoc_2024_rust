ca6fa5cb121df2cbde80a017d39f9cbef2be5105
Mikhail Batishchev <mikl181@yandex.ru>
2024-11-30 21:56:44 +0100

    initial commit

---
+++ src/__template_day_x.rs
@@ -0,0 +1,15 @@
+#[allow(dead_code)]
+fn solve() {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        solve();
+        assert!(true);
+    }
+}
